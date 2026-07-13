885134b0815293b1d5c46167ad4a7aa049430a7e
Pranjal kr Gautam <pranjalgautam!80@gmail.com>
2026-07-13 11:29:24 +0530

    localhost

--- project/src/config/api.js
+++ project/src/config/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://zomato-clone-production-f7ac.up.railway.app/';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://zomato-clone-production-f7ac.up.railway.app';
 
 export default API_BASE_URL;
